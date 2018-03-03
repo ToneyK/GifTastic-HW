30736eecf7ced0e0a3a76993245d2886f7ae7fb2
Your Name <you@example.com>
2018-03-02 18:50:15 -0700

    upload before tutoring

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+	$(function () {
+		var buttons = ["Dog", "Cat", "Parrot", "Horse"];
+		for (var i = 0; i < buttons.length; i++) {
+			$(".buttonHolder").append("<button class='animal'>" + buttons[i] + "</button>")
+		}
+
+		$("#addButton").on("click", function (event) {
+			event.preventDefault();
+
+			var buttonText = $("#buttonAdder").val().trim();
+			buttons.push(buttonText);
+
+			$(".buttonHolder").empty();
+			for (var i = 0; i < buttons.length; i++) {
+				$(".buttonHolder").append("<button class='animal'>" + buttons[i] + "</button>")
+			}
+		});
+
+		$(document).on("click", ".animal", function () {
+			var queryText = $(this).text();
+			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryText + "&api_key=dc6zaTOxFJmzC&limit=10";
+			console.log(queryURL);
+
+			$.ajax({
+				url: queryURL,
+				method: "GET"
+			}).then(function (response) {
+				console.log(response);
+				image.attr("src", results[i].images.fixed_height.url);
+				var results = response.data;
+				var rating = results[i].rating;
+				var p = $("<p>").text("Rating: " + rating);
+				var image = $("<img>");
+				var gifDiv = $("<div class='item'>");
+				gifDiv.prepend(p);
+				gifDiv.prepend(image);
+
+				$("#gifs-appear-here").prepend(gifDiv);
+
+			});
+		});
+
+
+
+
+
+	}); // END READY
