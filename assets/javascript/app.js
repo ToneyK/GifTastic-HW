2a51ba5b729b0f16d4586423f1f911dbbfc3da2d
Your Name <you@example.com>
2018-03-02 19:04:54 -0700

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,49 @@
-	$(function () {
-		var buttons = ["Dog", "Cat", "Parrot", "Horse"];
+//READY
+$(function () {
+	//LOOP TO CREAT BUTTONS
+	var buttons = ["Dog", "Cat", "Parrot", "Horse"];
+	for (var i = 0; i < buttons.length; i++) {
+		$(".buttonHolder").append("<button class='animal'>" + buttons[i] + "</button>")
+	}
+//INPUT BUTTON CREATOR FUNCTION
+	$("#addButton").on("click", function (event) {
+		event.preventDefault();
+
+		var buttonText = $("#buttonAdder").val().trim();
+		buttons.push(buttonText);
+
+		$(".buttonHolder").empty();
 		for (var i = 0; i < buttons.length; i++) {
 			$(".buttonHolder").append("<button class='animal'>" + buttons[i] + "</button>")
 		}
+	});
+//ON CLICK API CALL FUNCTION 
+	$(document).on("click", ".animal", function () {
+		var queryText = $(this).text();
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryText + "&api_key=dc6zaTOxFJmzC&limit=10";
+		console.log(queryURL);
 
-		$("#addButton").on("click", function (event) {
-			event.preventDefault();
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).then(function (response) {
+			console.log(response);
+			image.attr("src", results[i].images.fixed_height.url);
+			var results = response.data;
+			var rating = results[i].rating;
+			var p = $("<p>").text("Rating: " + rating);
+			var image = $("<img>");
+			var gifDiv = $("<div class='item'>");
+			gifDiv.prepend(p);
+			gifDiv.prepend(image);
 
-			var buttonText = $("#buttonAdder").val().trim();
-			buttons.push(buttonText);
+			$("#gifs-appear-here").prepend(gifDiv);
 
-			$(".buttonHolder").empty();
-			for (var i = 0; i < buttons.length; i++) {
-				$(".buttonHolder").append("<button class='animal'>" + buttons[i] + "</button>")
-			}
 		});
-
-		$(document).on("click", ".animal", function () {
-			var queryText = $(this).text();
-			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryText + "&api_key=dc6zaTOxFJmzC&limit=10";
-			console.log(queryURL);
-
-			$.ajax({
-				url: queryURL,
-				method: "GET"
-			}).then(function (response) {
-				console.log(response);
-				image.attr("src", results[i].images.fixed_height.url);
-				var results = response.data;
-				var rating = results[i].rating;
-				var p = $("<p>").text("Rating: " + rating);
-				var image = $("<img>");
-				var gifDiv = $("<div class='item'>");
-				gifDiv.prepend(p);
-				gifDiv.prepend(image);
-
-				$("#gifs-appear-here").prepend(gifDiv);
-
-			});
-		});
+	});
 
 
 
 
 
-	}); // END READY
+}); // END READY
